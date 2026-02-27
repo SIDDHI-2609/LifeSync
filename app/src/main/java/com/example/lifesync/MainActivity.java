9ea4c530a200086fcad3c6535ccd64cca4834146
SIDDHI-2609 <siddhi.sahu2609@gmail.com>
2026-02-27 22:57:16 +0530

    main

---
+++ app/src/main/java/com/example/lifesync/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lifesync;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle s) {
+        super.onCreate(s);
+        setContentView(R.layout.activity_main);
+    }
+}
